bd8e1e117c05c35ec04cf77df7107ae639366e6a
Andrew Zong <a.zong@f5.com>
2020-10-25 19:32:47 +0800

    Add release script.

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+./build.sh
+
+targetdir=f5-oslbaasv2-tools-release
+mkdir $targetdir
+
+for n in f5-oslbaasv2-batchops f5-oslbaasv2-parselog f5-oslbaasv2-taillog; do
+    for m in dist scripts; do
+        if [ -d $n/$m ]; then
+            cp -r $n/$m $targetdir
+        fi
+    done 
+done
+
+mkdir $targetdir/output
