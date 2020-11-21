5ea761e687a188ed52affe2e74e73a67743f61e7
root <root@overcloud-controller-0.localdomain>
2020-11-21 11:26:53 +0800

    add list-db-lbaasv2-objects.sh script

--- f5-oslbaasv2-batchops/scripts/create-members.sh
+++ f5-oslbaasv2-batchops/scripts/create-members.sh
@@ -22,13 +22,13 @@ project_name=proj_1
 loadbalancer=lb-1-1
 
 # pool: the member belongs to.
-pool=pl-1-1-7
+pool=pl-1-1-8
 
 # the range of members, will be used in member's address 
-mbrange=26-74
+mbrange=75-138
 
 # The ip address first bits.
-ip_prefix=1.1.7
+ip_prefix=1.1.8
 # ====================================================
 
 dts=`date +%Y-%m-%d-%H-%M-%S`
