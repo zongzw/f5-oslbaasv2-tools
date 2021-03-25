b94e9cdf45ab2943c17a71309b1127a4f5d3e047
Andrew Zong <a.zong@f5.com>
2021-03-25 12:23:24 +0800

    Add neutrondb_hostname.

--- f5-oslbaasv2-batchops/scripts/ansible-playbook/if-exists-resource.sh
+++ f5-oslbaasv2-batchops/scripts/ansible-playbook/if-exists-resource.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "arguments: <loadbalancers|listeners|pools|members|healthmonitors> <name> <dbpass> <dbname>"
+set -x 
+if [ $# -ne 5 ]; then
+    echo "arguments: <loadbalancers|listeners|pools|members|healthmonitors> <name> <dbhost> <dbpass> <dbname>"
     exit 1
 fi
 
-count=`echo "select count(*) as count from lbaas_$1 where name = '$2';" | mysql -uneutron -p$3 $4 | grep -v count`
+count=`echo "select count(*) as count from lbaas_$1 where name = '$2';" | mysql -uneutron -h$3 -p$4 $5 | grep -v count`
 if [ $? -ne 0 ]; then
     echo "failed to check count from db"
     exit 1
