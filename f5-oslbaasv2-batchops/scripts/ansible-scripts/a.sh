4e89144b5f672620daba75ebbbcf6ed08d03f9d7
Andrew ZONG <a.zong@f5.com>
2021-05-21 11:37:39 +0800

    little fix and a.sh for batch.

---
+++ f5-oslbaasv2-batchops/scripts/ansible-scripts/a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for n in `seq $1 $2`; do
+
+    ansible-playbook \
+        -i env-nodes.ini \
+        -e @vars-model-5.yml \
+        create-resources-in-batch.yml -e index=$n
+
+done
