18f66ee6e6fabc05e4f2b071e4ca0c7f9dae596e
Andrew Zong <a.zong@f5.com>
2021-08-03 01:18:22 +0000

    Add +x to a.sh

--- f5-oslbaasv2-batchops/scripts/ansible-scripts/a.sh
+++ f5-oslbaasv2-batchops/scripts/ansible-scripts/a.sh
@@ -3,7 +3,7 @@
 for n in `seq $1 $2`; do
 
     ansible-playbook \
-        -i env-nodes.ini \
+        -i env-lab.ini \
         -e @vars-model-5.yml \
         create-resources-in-batch.yml -e index=$n
 
