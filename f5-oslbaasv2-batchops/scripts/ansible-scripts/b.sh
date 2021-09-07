61552cc6be8efd617284d7066101d5aa3ec1e8a0
Andrew ZONG <a.zong@f5.com>
2021-09-07 15:44:33 +0800

    Add the group order creation scripts.

---
+++ f5-oslbaasv2-batchops/scripts/ansible-scripts/b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for n in `seq $1 $2`; do
+
+    ansible-playbook \
+        -i env-lab.ini \
+        -e @vars-model-5.yml \
+        create-resources-in-group.yml -e index=$n
+
+done
